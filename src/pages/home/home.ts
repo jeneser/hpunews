a2306912b367700cc9a1751d5f6e26fd51b602e9
jeneser <2867483044@qq.com>
2017-02-19 18:17:03 +0800

    update home component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  catalog: string = 'rec';
 
   constructor(public navCtrl: NavController) {
-    
+
   }
+
+  doRefresh(refresher) {
+    setTimeout(() => {
+      console.log('refresher ok!');
+      refresher.complete();
+    }, 2000);
+  }
+
+  doInfinite(infiniteScroll) {
+    setTimeout(() => {
+      console.log('infiniteScroll ok!');
+      infiniteScroll.complete();
+    }, 2000);
+  }
+
 
 }
