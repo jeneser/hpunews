d8f783c523fdf0722ad62b0086e8c7cf6090cac1
jeneser <2867483044@qq.com>
2017-02-21 21:37:17 +0800

    fix#3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SettingPage } from '../pages/setting/setting';
+import { JobPage } from '../pages/job/job';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SettingPage,
+    JobPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +18,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SettingPage,
+    JobPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,45 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Slides, Tabs, Tab } from 'ionic-angular';
+
+import { SettingPage } from '../setting/setting';
+import { JobPage } from '../job/job';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-  catalogArray = ['rec', 'news', 'logistic', 'notice', 'job'];
-  catalog: string = this.catalogArray[0];
+
+export class HomePage implements AfterViewInit {
+  @ViewChild(Slides) slider: Slides;
+  @ViewChild('myTabs') tabRef: Tabs;
+
+  tab1Root = JobPage;
+  tab2Root = JobPage;
+  tab3Root = JobPage;
+
+  pushPage: any;
 
   constructor(public navCtrl: NavController) {
+    this.pushPage = SettingPage;
+  }
+
+  ionViewDidEnter() {
+    this.tabRef.select(0);
+  }
+
+  ngAfterViewInit() {
 
   }
 
-  doRefresh(refresher) {
-    setTimeout(() => {
-      console.log('refresher ok!');
-      refresher.complete();
-    }, 2000);
-  }
 
-  doInfinite(infiniteScroll) {
-    setTimeout(() => {
-      console.log('infiniteScroll ok!');
-      infiniteScroll.complete();
-    }, 2000);
-  }
-
-  swipeEvent(event) {
-    // 左滑
-    if (event.direction === 2) {
-      if (this.catalogArray.indexOf(this.catalog) < this.catalogArray.length - 1) {
-        this.catalog = this.catalogArray[this.catalogArray.indexOf(this.catalog) + 1];
-      }
+  onTabsChange(ev: Tab) {
+    var tab_index = ev.index;
+    var slider_index = this.slider.getActiveIndex();
+    if (tab_index != slider_index) {
+      this.slider.slideTo(tab_index);
     }
-    // 右滑
-    if (event.direction === 4) {
-      if (this.catalogArray.indexOf(this.catalog) > 0) {
-        this.catalog = this.catalogArray[this.catalogArray.indexOf(this.catalog) - 1];
-      }
+  }
+
+  onSlideDidChange(s: Slides) {
+    var slider_index = s.getActiveIndex();
+    var tab_index = this.tabRef.getSelected().index;
+    if (tab_index != slider_index) {
+      this.tabRef.select(slider_index);
     }
   }
 
