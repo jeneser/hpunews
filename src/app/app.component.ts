96322e2bb064a103e1ae3170299a4b494e15e940
jeneser <2867483044@qq.com>
2017-03-10 12:13:29 +0800

    add menu link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,15 @@ import { Platform, Nav } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { FavoritePage } from '../pages/favorite/favorite';
+import { FocusPage } from '../pages/focus/focus';
+import { LogisticsPage } from '../pages/logistics/logistics';
+import { NoticePage } from '../pages/notice/notice';
+import { ClubPage } from '../pages/club/club';
 import { JobPage } from '../pages/job/job';
+import { AccountPage } from '../pages/account/account';
 import { SettingPage } from '../pages/setting/setting';
-
+import { AboutPage } from '../pages/about/about';
 
 @Component({
   templateUrl: 'app.html'
@@ -14,19 +20,28 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
-
-  pages: Array<{title: string, component: any}>;
+  focusPages: Array<{title: string, component: any, icon: string}>;
+  tagPages: Array<{title: string, component: any, icon: string}>;
+  otherPages: Array<{title: string, component: any, icon: string}>;
 
   constructor(public platform: Platform) {
     this.initializeApp();
 
-    this.pages = [
-      { title: '要闻', component: HomePage },
-      { title: '后勤', component: JobPage },
-      { title: '公告', component: JobPage },
-      { title: '学院', component: JobPage },
-      { title: '招聘', component: JobPage },
-      { title: '设置', component: SettingPage }
+    this.focusPages = [
+      { title: '理工大事件', component: HomePage, icon: 'md-cube' },
+      { title: '我感兴趣的', component: FavoritePage, icon: 'md-analytics' }
+    ];
+    this.tagPages = [
+      { title: '要闻', component: FocusPage, icon: 'md-list-box' },
+      { title: '后勤', component: LogisticsPage, icon: 'md-bookmarks' },
+      { title: '公告', component: NoticePage, icon: 'md-archive' },
+      { title: '社团', component: ClubPage, icon: 'md-chatboxes' },
+      { title: '招聘', component: JobPage, icon: 'md-briefcase' }
+    ];
+    this.otherPages = [
+      { title: '账号', component: AccountPage, icon: 'md-person' },
+      { title: '设置', component: SettingPage, icon: 'md-settings' },
+      { title: '关于', component: AboutPage, icon: 'md-alert' }
     ];
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { SettingPage } from '../pages/setting/setting';
+import { FavoritePage } from '../pages/favorite/favorite';
+import { FocusPage } from '../pages/focus/focus';
+import { LogisticsPage } from '../pages/logistics/logistics';
+import { NoticePage } from '../pages/notice/notice';
+import { ClubPage } from '../pages/club/club';
 import { JobPage } from '../pages/job/job';
+import { AccountPage } from '../pages/account/account';
+import { SettingPage } from '../pages/setting/setting';
+import { AboutPage } from '../pages/about/about';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
+    FavoritePage,
+    FocusPage,
+    LogisticsPage,
+    NoticePage,
+    ClubPage,
+    JobPage,
+    AccountPage,
     SettingPage,
-    JobPage
+    AboutPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -19,8 +33,15 @@ import { JobPage } from '../pages/job/job';
   entryComponents: [
     MyApp,
     HomePage,
+    FavoritePage,
+    FocusPage,
+    LogisticsPage,
+    NoticePage,
+    ClubPage,
+    JobPage,
+    AccountPage,
     SettingPage,
-    JobPage
+    AboutPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
