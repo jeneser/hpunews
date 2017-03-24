191cce6ac7d225c46035eac615747acd444e7b28
jeneser <jeneserwang@gmail.com>
2017-03-24 23:24:31 +0800

    edit notice layout and styles

--- src/pages/notice/notice.ts
+++ src/pages/notice/notice.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Notice page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-notice',
   templateUrl: 'notice.html'
 })
 export class NoticePage {
+  classify: string = 'notice';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
